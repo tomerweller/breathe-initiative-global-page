9119b6bb8f9716da85e4d9d02fb6e81fc16c3d99
LarissaKT <lak06@hs-lak06.ic.ac.uk>
2015-08-24 16:55:04 +0100

    Hackathon Website
    The ICAH repository from before. Hackathon website.

---
+++ index.php
@@ -0,0 +1,429 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+	<meta content="width=device-width, initial-scale=1.0" name="viewport" />
+	<title>BREATHE Respiratory Hackathon</title>
+	<link href="pure-min.css" rel="stylesheet" type="text/css" />
+	<link href="font-awesome.css" rel="stylesheet" type="text/css" />
+	<link href="style.css" rel="stylesheet" type="text/css" />
+	<link href="img/favicon.ico" rel="shortcut icon" /><script src="https://maps.googleapis.com/maps/api/js">
+<script> function initialize(){
+	var mapCanvas = document.getElementById('map-canvas');
+    var mapOptions = {
+		center: new google.maps.LatLng(51.499368, -0.174661),
+		zoom:8,
+		mapTypeId: google.maps.MayTypeId.ROADMAP
+	var map = new google.maps.Map(mapCanvas);
+	}
+    google.maps.event.addDomListener(window, 'load', initialize)
+    </script>
+<!--    
+<style>
+  #map {
+    width: 500px;
+    height: 400px;
+    background-color: #CCC;
+  }
+</style>
+<script src="https://maps.googleapis.com/maps/api/js"></script>
+<script>
+      function initialize() {
+        var mapCanvas = document.getElementById('map');
+        var mapOptions = {
+          center: new google.maps.LatLng(51.499368, -0.174661),
+          zoom: 17,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+       
+        var map = new google.maps.Map(mapCanvas, mapOptions)
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+    </script>
+    -->
+    <!-- 44.5403, -78.5463
+<script type="text/javascript" async="" src="http://www.google-analytics.com/plugins/ua/linkid.js"></script>
+<script async="" src="//www.google-analytics.com/analytics.js"></script>
+
+<script src="js/bootstrap-built.js"></script>    
+<script type="text/javascript" src="//cdn.syndication.twimg.com/widgets/timelines/44056764343557…callback&suppress_response_codes=true&rnd=0.3101157261096634"></script>
+-->
+</head>
+<body><!-- ------------------------------------------Links to other pages at the top
+    
+    <div class="site-menu">
+	<div class="pure-g-r">
+	<a href="#locations" data-menuanchor="locations" class="pure-u-1-4">Locations</a>
+	<a href="#prizes" data-menuanchor="prizes" class="pure-u-1-4">Prize Winners '15</a>
+	<a href="#sponsors" data-menuanchor="sponsors" class="pure-u-1-4">Sponsor '16</a>
+	<a href="#contact" data-menuanchor="contact" class="pure-u-1-4">Contact</a></div></div>
+    
+    ------------------------------------------  --><!-- ------------------------------------------Video / image in the background------------------------------------------  -->
+<div class="content">
+<div class="heroVideo" style="position: fixed; top: -90px; z-index: -99; width: 100%; height: 100%; color:"><!--<iframe frameborder="0" height="100%" width="100%" src="https://youtube.com/embed/YPydEfw0wHE?autoplay=1&amp;controls=0&amp;showinfo=0&amp;autohide=1&amp;modestbranding=1&amp;disablekb=1&amp;loop=1&amp;playlist=YPydEfw0wHE"></iframe> --><!--	<img src="images/Color Blue Hd Cool 7 HD Wallpapers.jpg" /> --><img src="images/BlueWP.jpg" /></div>
+
+<div class="heroOverlay">&nbsp;</div>
+
+<section class="section heroSection" data-anchor="ichack">
+<div class="pure-g-r">
+<div class="pure-u-1">
+<div class="pure-g">
+<div class="pure-u-5-6 center">
+<div class="hero-logo"><img alt="Breathe" class="hack_logo" src="images/BreatheLogo_Horozontal_White.png" /></div>
+</div>
+</div>
+</div>
+<!--
+<div class="pure-u-1-2">
+<div class="pure-g">
+<div class="pure-u-5-6 center">
+<div class="hero-catch-phrase">
+<h2>The Global<br />
+Respiratory Hackathon</h2>
+</div>
+</div>
+</div>
+</div>
+    -->
+ <!-- 
+-->
+</div>
+</section>
+<!-- ------------------------------------------Introductory text with small embedded video------------------------------------------  -->
+
+<section class="section darkSection" data-anchor="about">
+<div class="pure-g-r">
+<div class="pure-u-1 largeSectionHeadline">
+<h1>Join us this September!</h1>
+</div>
+
+<div class="pure-u-1">
+<div class="free-text">
+<h3>Hackathon for respiratory patients.</h3>
+
+<p>Imperial College Advanced Hackspace (ICAH) presents its largest hackathon yet. A global event which will take place simultaneously across three continents - at ICAH in London, UK; MIT Media Lab in Boston, USA and at Technion – Israel Institute of Technology in Haifa, Israel.</p>
+
+<p>Dubbed the BREATHE Respiratory Hackathons, these events will bring together designers, technicians, engineers, patients, doctors and creative thinking entrepreneurs and innovators to develop synergistic innovations and devices with patient-centered wellness and treatment as the focus.</p>
+
+<p>Teams will be given free rein to explore solutions to many of the problems respiratory sufferers face daily. That may include integrating multiple devices for nighttime and morning use, making existing equipment lighter weight, adding Bluetooth interfaces to equipment to allow control through a smartphone, or any number of ideas that push the status quo and challenge the respiratory industry to innovate around patient-centered wellness.</p>
+
+<p>Results from all three Hackathons will be shared with the media and 22,000+ attendees at the <a style="color: #3399FF" href="http://www.erscongress.org/">European Respiratory Society Conference</a> in Amsterdam September 27, 2015.</p>
+<!--    
+    <p>Don't forget to watch the video!</p>
+    <div class="videoThumb"><a target="_blank" href="https://www.youtube.com/watch?v=C7lnm1-ZYk4" style="height: 174px" class="fade-hover"><img src="/img/videothumb.jpg"><i class="fa fa-play"></i></a></div>
+    <p style="margin-bottom:0" class="verticalSpaceSmall">With love,</p>
+    <a href="http://twitter.com/ICDoCSoc">DoCSoc</a>
+    <p class="smallText">PS: We're looking for sponsors for IC Hack '16! 
+    <a href="#sponsors">Learn more</a>.</p>
+        </div>
+        --></div>
+</div>
+</div>
+</section>
+<!-- ------------------------------------------Details------------------------------------------  -->
+
+<section class="section sponsorSection" data-anchor="details">
+<div class="pure-g-r">
+<div class="pure-u-1">
+<div class="largeSectionHeadline">
+<div id="tc_3719"><a title="Countdown" href="http://www.tickcounter.com/countdown/20150919090000am/w4410/0/">Countdown</a> <a title="Countdown online" href="http://www.tickcounter.com">Countdown</a></div><script type="text/javascript">(function(){var s = document.createElement("script");s.src = "http://www.tickcounter.com/widget/countdown/20150919090000am/w4410/0/3719/1000/222222ffffffffffff/";s.async = "async";s.type = "text/javascript";var x = document.getElementById("tc_3719");x.insertBefore(s,x.lastChild);}());</script>
+&nbsp
+&nbsp
+<h1>Details</h1>
+</div>
+</div>
+
+<div class="pure-u-1">
+<div class="prize">
+<div class="prize-value">Date</div>
+
+<div class="prize-description">Saturday 19th - Sunday 20th September 2015</div>
+</div>
+</div>
+
+<div class="pure-u-1">
+<div class="prize">
+<div class="prize-value">Location</div>
+
+<div class="prize-description">Skempton Building<br />
+Imperial College London<br />
+Imperial College Road<br />
+London SW7 2AZ</div>
+
+<iframe src="https://www.google.com/maps/d/embed?mid=z1olQOV4M4X4.kVsvr7fpBlDc&zoom=15" width="640" height="480"></iframe>
+<!--
+<div id="map-canvas">&nbsp;</div>
+
+<div id="map"></div>-->
+
+<div class="pure-u-1">
+<div class="prize">
+<div class="prize-value">Sign up Link Coming Soon</div>
+</div>
+</div>
+
+</div>
+</div>
+
+<!--
+<div class="pure-u-1-2">
+<div class="prize">
+<div class="prize-sponsor">I&#39;m an</div>
+
+<div class="prize-value">Engineer.</div>
+
+<div class="prize-description">I want to join.</div>
+</div>
+</div>
+
+<div class="pure-u-1-2">
+<div class="prize">
+<div class="prize-sponsor">I&#39;m a</div>
+
+<div class="prize-value">Doctor.</div>
+
+<div class="prize-description">I want to join.</div>
+</div>
+</div>
+
+<div class="pure-u-1-2">
+<div class="prize">
+<div class="prize-sponsor">I&#39;m a</div>
+
+<div class="prize-value">Patient.</div>
+
+<div class="prize-description">I want to join.</div>
+</div>
+</div>
+
+<div class="pure-u-1-2">
+<div class="prize">
+<div class="prize-sponsor">I&#39;m a</div>
+
+<div class="prize-value">Designer.</div>
+
+<div class="prize-description">I want to join.</div>
+</div>
+</div>
+
+<div class="pure-u-1-2">
+<div class="prize">
+<div class="prize-sponsor">I&#39;m a</div>
+
+<div class="prize-value">Technician.</div>
+
+<div class="prize-description">I want to join.</div>
+</div>
+</div>
+
+<div class="pure-u-1-2">
+<div class="prize">
+<div class="prize-sponsor">I have</div>
+
+<div class="prize-value">Other Skills.</div>
+
+<div class="prize-description">I want to join.</div>
+</div>
+</div>
+
+<div class="pure-u-1-1">
+<div class="prize">
+<div class="prize-value">I want to</div>
+
+<div class="prize-description">Find out more</div>
+</div>
+</div>
+</div>
+-->
+
+</section>
+<!-- ------------------------------------------Prizes------------------------------------------  -->
+
+<section class="section blueSection" data-anchor="prizes">
+<div class="pure-g-r">
+<div class="pure-u-1">
+<div class="largeSectionHeadline">
+<h1>Prizes</h1>
+</div>
+</div>
+
+<div class="pure-u-1">
+<div class="prize">
+<div class="prize-value">Prizes will be announced soon</div>
+</div>
+</div>
+
+<!--
+<div class="pure-u-1">
+<div class="prize">
+<div class="prize-value">Prize 1 value</div>
+
+<div class="prize-description">Prize 1 description</div>
+
+<div class="prize-sponsor">Prize 1 sponsor</div>
+</div>
+</div>
+
+<div class="pure-u-1-2">
+<div class="prize">
+<div class="prize-value">Prize 2 value</div>
+
+<div class="prize-description">Prize 2 description</div>
+
+<div class="prize-sponsor">Prize 2 sponsor</div>
+</div>
+</div>
+
+<div class="pure-u-1-2">
+<div class="prize">
+<div class="prize-value">Prize 3 value</div>
+
+<div class="prize-description">Prize 3 description</div>
+
+<div class="prize-sponsor">Prize 3 sponsor</div>
+</div>
+</div>
+//////////////////////////////
+-->
+
+
+<!--
+<div class="pure-u-1-2">
+<div class="prize">
+<div class="prize-value">Prize 4 value</div>
+
+<div class="prize-description">Prize 4 description</div>
+
+<div class="prize-sponsor">Prize 4 sponsor</div>
+</div>
+</div>
+
+<div class="pure-u-1-2">
+<div class="prize">
+<div class="prize-value">Prize 5 value</div>
+
+<div class="prize-description">Prize 5 description</div>
+
+<div class="prize-sponsor">Prize 5 sponsor</div>
+</div>
+</div>
+-->
+</div>
+</section>
+<!-- ------------------------------------------Sponsors------------------------------------------  -->
+
+<section class="section darkSection" data-anchor="sponsors" style="padding-top: 20px">
+<div class="pure-g-r sponsors" style="padding-top: 20px">
+<div class="pure-u-1 smallSectionHeadline verticalSpaceSmall">
+<h1>Breathe Respiratory Hackathon is sponsored by</h1>
+</div>
+<!--
+		<div class="pure-u-1">
+			<div class="free-text">
+			<p>IC Hack (Imperial College Hackathon) is an annual event held during the spring term. It is run by the <a href="http://docsoc.co.uk">Department of Computing Society</a> and is the largest student-run technology event at Imperial.</p>
+			<p>At IC Hack '15, 150 hackers from Imperial and around the UK came together for a weekend of digital creativity! Next year we want to super-size IC Hack - more students, more projects and more prizes! We're looking for industry partners whose engineers can join the hackathon as mentors, inspire creativity and provide technical insight to atttendees.</p>
+			<p>Sponsorship tier pricing for IC Hack '16 is to be confirmed. Benefits will include</p>
+
+			<ul>
+				<li>Judge the hacks and sponsor prizes.</li>
+				<li>Send engineers from your company.</li>
+				<li>Send promotional material and swag to the hackathon.</li>
+				<li>Branding on the website and at the venue.</li>
+				<li>Short presentation/tech demonstration before the hackathon commences.</li>
+				<li>Run a tech-focused workshop during the hackathon.</li>
+			</ul>
+
+			<p>Be a part of IC Hack '16 from the start, email us at <a href="mailto:info@ichack.org">info@ichack.org</a> if you're interested.</p>
+			</div>
+		</div>
+
+		<div class="pure-u-1 smallSectionHeadline verticalSpaceSmall">
+		<h1>IC Hack '15 was sponsored by</h1>
+		</div>
+-->
+<!--
+<div class="pure-u-1 smallSectionHeadline verticalSpaceSmall"><h1>Main Sponsor</h1></div>
+<div class="pure-u-1-5"></div>
+<div class="pure-u-1-5"></div>
+<a target="_blank" style="background-image" href="https://www.novartis.com/" class="pure-u-1-5"><img src="images/sponsors/Novartis.png")></a>
+<div class="pure-u-1-5"></div>
+<div class="pure-u-1-5"></div>
+
+<div class="pure-u-1"></div>
+<div class="pure-u-1-5"></div>
+<div class="pure-u-1-5 smallSectionHeadline verticalSpaceSmall"><h1>Supported By</h1></div>
+<div class="pure-u-1-5 smallSectionHeadline verticalSpaceSmall"><h1>Partnered With</h1></div>
+<div class="pure-u-1-5"></div>
+<div class="pure-u-1-5"></div>
+
+<div class="pure-u-1"></div>
+<div class="pure-u-1-5"></div>
+<a target="_blank" href="http://www.cambridgeconsultants.com/" class="pure-u-1-5"><img width="80%" height="80%" src="images/sponsors/CC.png"/></a>
+<a target="_blank" href="http://www.copdfoundation.org/" class="pure-u-1-5"><img src="images/sponsors/COPD.png"/></a>
+<a target="_blank" href="https://www.blf.org.uk/Home" class="pure-u-1-5"><img src="images/sponsors/BLF.png" width="70%" height="70%"/></a>
+<div class="pure-u-1-5"></div>
+
+<div class="pure-u-1"></div>
+<div class="pure-u-1 smallSectionHeadline verticalSpaceSmall"><h1>Equipment Sponsored by</h1></div>
+<div class="pure-u-1-6"></div>
+<a target="_blank" href="http://www.respironics.com/" class="pure-u-1-6"><img src="images/sponsors/Philips_Respironics.png"/></a>
+<a target="_blank" href="http://www.sensirion.com/en/home/" class="pure-u-1-6"><img src="images/sponsors/Sensirion.gif"/></a>
+<div class="pure-u-1-8"></div>
+<a target="_blank" href="http://www.resmed.com/uk/en/consumer.html" class="pure-u-1-6"><img src="images/sponsors/ResMed.png" /></a>
+<a target="_blank" href="http://www.inovalabs.com/" class="pure-u-1-6"><img src="images/sponsors/InovaLabs.jpg"/></a>
+<div class="pure-u-1-6"></div>
+
+</div>
+</section>
+-->
+<!-- ------------------------------------------Other Events------------------------------------------  -->
+<!--
+<section class="section sponsorSection" data-anchor="details">
+<div class="pure-g-r">
+<div class="pure-u-1">
+<div class="largeSectionHeadline">
+
+
+<h1>is proud to host this event with the events at:</h1>
+
+<div class="pure-u-1-4"></div>
+<a target="_blank" style="background-image" href="" class="pure-u-1-4"><img src="images/sponsors/Novartis.png")></a>
+<a target="_blank" style="background-image" href="" class="pure-u-1-4"><img src="images/sponsors/Novartis.png")></a>
+<div class="pure-u-1-4"></div>
+
+
+
+</div>
+</div>
+-->
+<!-- ------------------------------------------Contact------------------------------------------  -->
+<!--
+<section class="section sponsorSection orangeSection" data-anchor="contact">
+<div class="pure-g-r">
+<div class="pure-u-2-3 center">
+<div class="smallSectionHeadline">
+<h1>Contact</h1>
+</div>
+
+<div class="contact-text">Email us at <a href="mailto:info@ichack.org">XXX</a></div>
+</div>
+</div>
+</section>
+-->
+<section class="section footerSection orangeSection">
+<div class="pure-g-r">
+<div class="pure-u-2-3 center">Organised by <a href="http://www.icah.org.uk">ICAH</a> - Imperial College Advanced Hackspace</div>
+
+<div class="pure-u-2-3 center">By LarissaKT - Based on a design by <a href="http://github.com/nkp">nkp</a> and <a href="http://github.com/xixixao">xixixao</a></div>
+</div>
+</section>
+</div>
+</body>
+<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-35567252-2', 'ichack.org');
+ga('require', 'linkid', 'linkid.js');
+ga('send', 'pageview');</script></html>
